0c92c2c8dabdbc9ea016f4fb0dff03105df9c0c4
NadiasCodes <De.nadia@outlook.com>
2023-12-22 12:43:00 +0100

    added a function where everytime someone clicks, the bg changes along with h1, and containers

--- src/java.js
+++ src/java.js
@@ -1,6 +1,22 @@
+function changeTheme() {
+  let body = document.querySelector("body");
+  if (body.classList.contains("christmas")) {
+    body.classList.remove("christmas");
+  } else {
+    body.classList.add("christmas");
+  }
+  let header = document.querySelector("h1");
+  header.innerHTML = "Merry Christmas My Love";
+  if (h1.classList.contains("christmas")) {
+    h1.classList.remove("christmas");
+  } else {
+    h1.classList.add("christmas");
+  }
+}
 
+let themeButton = document.querySelector(".surprise-button");
+themeButton.addEventListener("click", changeTheme);
 
+let recipeFormElement = document.querySelector("#recipe-generator");
+recipeFormElement;
 
-
-let recipeForm = document.querySelector("#recipe-generator");
-recipeForm.addEventListener("submit", generateRecipe);
