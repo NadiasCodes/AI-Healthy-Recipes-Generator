a6ef72fda9a272332d007a9dccf44b418359a4f1
NadiasCodes <De.nadia@outlook.com>
2023-12-22 13:17:25 +0100

    created an ai function which generates healthy recipes

--- src/java.js
+++ src/java.js
@@ -1,12 +1,23 @@
-function generateRecipe(event) {
-  event.preventDefault();
-  let recipeElement = document.querySelector("#text");
+function displayRecipes(response) {
   new Typewriter("#text", {
-    strings: ["Recipes"],
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateRecipe(event) {
+  event.preventDefault();
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "57t510363ca64d76cf8d437ao0eea1eb";
+
+  let context =
+    "You are an expert AI nutritionist, who knows everything about the most healthy recipes especially with protein. Your mission is to generate all kind of healthy recipes especially with protein in basic HTML. Make sure to follow the user instructions below. Also always sign in the end by saying to enjoy their meal";
+  let prompt = `User instructions are: Generate a healthy recipe about ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayRecipes);
 }
 
 function changeTheme() {
